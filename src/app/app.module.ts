ca2b53b8b8bc1b61f048cde04211476cd5a1aec0
Ganguly, Biswajit <bganguly@deloitte.com>
2018-06-22 13:38:40 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftnavComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
